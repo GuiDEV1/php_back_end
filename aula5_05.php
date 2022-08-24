ef2e0584d3c45cad5b398315b1dd3c1b71902f6e
Guilherme <guilhermemascarenhas21g@gmail.com>
2022-08-24 19:49:29 -0300

    aula2

---
+++ aula5_05.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>código 02</title>
+</head>
+<body>
+    <h1>PHP</h1> 
+    <?php
+    $_nome = "João antonio";
+    $Nome = "Alfredo Maciel";
+    $valor = 123456;
+    $nota = 7.5;
+   
+    print ($valor);
+
+     ?>
+</body>
+</html>
