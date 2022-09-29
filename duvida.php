48321c975adc12b4d6ef4d3b5b8ff2dea6322c10
Guilherme <guilhermemascarenhas21g@gmail.com>
2022-09-28 21:38:40 -0300

    duvidas

---
+++ duvida.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Duvidas php</title>
+</head>
+<body>
+    <?php
+/*  $a = 10;
+    $b= 4;
+    $c = $a%$b;
+    echo $c;
+    **********************
+    // constante
+
+    define("ANO", 2022);
+    
+    $a = 5;
+    $a = ANO+5; 
+    echo $a;
+  
+  
+  $i = 10;
+  $i+= 2; // 12
+  $b = 3;
+  $b -= 2; //3
+  $i /= $b;
+
+  echo "i= $i";
+ */
+ 
+ $cep = "03312-123";
+ $cidade = "São paulo";
+ $endereco = $cep . ' - '. $cidade;
+
+ echo $endereco;
+ 
+
+    
+    ?>
+</body>
+</html>
